40c6f02f8eb1e7bf2c07109d3a904bcce43dbe66
jonas <jonas@WKS>
2015-06-14 13:52:41 +0200

    Working on RDFSpout

--- src/main/java/de/unikoblenz/west/lda/trees/spout/RDFSpout.java
+++ src/main/java/de/unikoblenz/west/lda/trees/spout/RDFSpout.java
@@ -1,5 +1,6 @@
 package de.unikoblenz.west.lda.trees.spout;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -11,10 +12,52 @@ import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
 
+import org.semanticweb.yars.nx.Node;
+import org.semanticweb.yars.nx.parser.*;
+
+
+
 public class RDFSpout extends BaseRichSpout {
 	private static final long serialVersionUID = 1L;
+	private static final int maxPackageSize = 50;
 	
 	SpoutOutputCollector collector;
+    InputStream input;
+    String fileLocation;
+    NxParser nxp;
+    
+    public RDFSpout(InputStream input){
+    	this.input = input;
+    	nxp = new NxParser(input);
+    }
+    
+    public RDFSpout(){
+    	
+    }
+    
+    public ArrayList<String> pack(){
+    	
+    	ArrayList<String> pack = new ArrayList<String>();
+    	
+    	String subject;
+    	String predicate;
+    	String object;
+    	
+    	Node[] line;
+    	
+    	while(pack.size()< maxPackageSize && nxp.hasNext()){
+    		line = nxp.next();
+    		
+    	}
+    	
+		
+    	return null;
+    	
+    }
+    
+    
+	
+	
 
 	public void open(@SuppressWarnings("rawtypes") Map conf, TopologyContext context,
 			SpoutOutputCollector collector) {
