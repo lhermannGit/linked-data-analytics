6d0f556ded3bb774b888cdc385c5d64cb5fff09c
mkrnr <info@mkoerner.de>
2015-06-28 20:02:35 +0200

    Rename treeGeneration package to match package structure

--- src/main/java/de/unikoblenz/west/lda/treeGeneration/ChildNode.java
+++ src/main/java/de/unikoblenz/west/lda/treeGeneration/ChildNode.java
@@ -1,61 +1,56 @@
-package treeGeneration;
+package de.unikoblenz.west.lda.treeGeneration;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Rootnode implements Node{
+public class ChildNode implements Node{
 
 	private int name;
+	private int predicate;
 	private List<ChildNode> children;
-	private int priority;
-	private int size;
+	//private Rootnode root; //really necessary??
 	
-	public Rootnode(int name){
+	
+	//public ChildNode(int name, int predicate, Rootnode root){
+	public ChildNode(int name, int predicate){
 		this.name = name;
+		this.predicate = predicate;
 		children = new ArrayList<ChildNode>();
-		size = 0;
-		priority = 100;
+		//this.root = root;
 	}
 	
 	public int getName(){
 		return name;
 	}
 	
-	public int getPriority(){
-		return priority;
+	public int getPredicate(){
+		return predicate;
 	}
 	
-	public int getSize(){
-		return size;
+	public void addChild(ChildNode child){
+		children.add(child);
+//		root.sucSize();
 	}
 	
+	// not yet needed
 	public List<ChildNode> getChildren(){
 		return children;
 	}
-	
-	public void addChild(ChildNode childNode){
-		children.add(childNode);
-		size++;
-	}
-	
-	public void sucSize(){
-		size++;
-	}
-	
+
 	// look if subject already exists as an object in children
 	// if found: create and add new ChildNode and add it to insertedNodes 
 	public void addIfInside(int subject, int pred, int object, List<ChildNode> insertedNodes){
-		if (this.name == subject) {
+		if (this.name == subject){
 			ChildNode newNode = new ChildNode(object,pred);
 			children.add(newNode);
 			insertedNodes.add(newNode);
-			size++;
-			// TODO: fstm.sendSubtree(newNode,this);
-			System.out.println("added new Child "+ object + " with predicate "+ pred + " to rootnode "+ this.name);
+//			root.sucSize();
+			// TODO: fstm.sendSubtree(newNode,root);
+			System.out.println("added new Child "+ object + " with predicate "+ pred + " to node " + this.name);
 		}
-		
 		for (ChildNode k: children){
 			k.addIfInside(subject, pred, object, insertedNodes);
-		}			
+		}
 	}
 }
+
