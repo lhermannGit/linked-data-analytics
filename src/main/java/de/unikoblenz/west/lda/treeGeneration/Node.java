6e9b6485d6c5f57d7912d7058bcdb8de9b2e4703
mkrnr <info@mkoerner.de>
2015-07-07 12:25:31 +0200

    Move filePath for Window to parameters of main function

--- src/main/java/de/unikoblenz/west/lda/treeGeneration/Node.java
+++ src/main/java/de/unikoblenz/west/lda/treeGeneration/Node.java
@@ -3,8 +3,15 @@ package de.unikoblenz.west.lda.treeGeneration;
 import java.util.List;
 
 interface Node {
-	//public void addNodesToList(int node, List<Node> result);
-	public void addChild(ChildNode childNode);
+
 	public int getName();
-	public void addIfInside(int subject, int pred, int object, List<ChildNode> insertedNodes);
+
+	public List<ChildNode> getChildren();
+
+	// public void addNodesToList(int node, List<Node> result);
+	public void addChild(ChildNode childNode);
+
+	public void addIfInside(int subject, int pred, int object,
+			List<ChildNode> insertedNodes);
+
 }
