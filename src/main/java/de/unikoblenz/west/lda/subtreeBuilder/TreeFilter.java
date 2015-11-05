c5ed1296f5c780e67364f6d08ba9d66bf0e7727d
eugenszk <e.saizki@gmail.com>
2015-11-05 15:53:50 +0100

    Look in Cache for Parent's Path and save results in list

--- src/main/java/de/unikoblenz/west/lda/subtreeBuilder/TreeFilter.java
+++ src/main/java/de/unikoblenz/west/lda/subtreeBuilder/TreeFilter.java
@@ -1,21 +1,35 @@
 package de.unikoblenz.west.lda.subtreeBuilder;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class TreeFilter {
-
 	// Getting Path from TreeTraversal
-	String path = "";
+	static String path = "1/2";
+
 	// String path = getPath();
 
 	// Querying Cache for Paths including just received Path
 
-	List<String> list = null;
+	List<String> list = new LinkedList<String>();
 
 	public void queryCache(String path) {
 		// first item is the new path
 		list.add(path);
 		// following items are the results from the cache
+		if (!path.contains("/")){
+			//return everything b/c we are looking for root node
+			//Cache.query(Nil);
+		} else
+			path = path.substring(0, path.lastIndexOf("/"));
+		System.out.println("TF: "+path);
+		
+		//query Cache for existing trees
+		//Cache.query(path)
+		
+		//Adding results from Cache to List
+		//list.addAll(Cache.query(ancestors)); 
+		
 
 		// handing created list over to TreeInserter
 		handToTreeInserter(list);
@@ -23,11 +37,17 @@ public class TreeFilter {
 
 	// Giving found Subtrees and Path to the TreeInserter
 	private void handToTreeInserter(List<String> list) {
-		if (list != null) {
-			TreeInserter.createTrees(list);
+		if (!list.isEmpty()) {
+			TreeInserter ti = new TreeInserter();
+			ti.createTrees(list);
 		}
 	}
 	/*
 	 * public List<String> getList(){ return list; }
 	 */
+
+	public static void main(String[] args) {
+		TreeFilter tf = new TreeFilter();
+		tf.queryCache(path);
+	}
 }
--- src/main/java/de/unikoblenz/west/lda/subtreeBuilder/TreeInserter.java
+++ src/main/java/de/unikoblenz/west/lda/subtreeBuilder/TreeInserter.java
@@ -7,8 +7,8 @@ public class TreeInserter {
 	//Getting or querying found Subtrees and new Path from TreeFilter
 	//List<String> list = TreeFilter.getList();
 	//Finding correct Positions for new Path
-	public static void createTrees (List<String> list){
-		
+	public void createTrees (List<String> list){
+		System.out.println("TI: "+list.toString());
 	}
 	//Discarding found Subtrees and saving newly created ones
 	
