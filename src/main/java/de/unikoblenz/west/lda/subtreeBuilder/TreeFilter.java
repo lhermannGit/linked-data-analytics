5957abba2a985071befe52833ea2904226b2bfdd
eugenszk <e.saizki@gmail.com>
2015-11-05 17:05:21 +0100

    Optimized and added transformation for alternative paths

--- src/main/java/de/unikoblenz/west/lda/subtreeBuilder/TreeFilter.java
+++ src/main/java/de/unikoblenz/west/lda/subtreeBuilder/TreeFilter.java
@@ -14,15 +14,14 @@ public class TreeFilter {
 	List<String> list = new LinkedList<String>();
 
 	public void queryCache(String path) {
-		// first item is the new path
-		list.add(path);
-		// following items are the results from the cache
+		
+		// getting results results from the cache
 		if (!path.contains("/")){
 			//return everything b/c we are looking for root node
 			//Cache.query(Nil);
 		} else
 			path = path.substring(0, path.lastIndexOf("/"));
-		System.out.println("TF: "+path);
+		System.out.println("TF - Path: "+path);
 		
 		//query Cache for existing trees
 		//Cache.query(path)
@@ -30,18 +29,33 @@ public class TreeFilter {
 		//Adding results from Cache to List
 		//list.addAll(Cache.query(ancestors)); 
 		
-
+		
 		// handing created list over to TreeInserter
 		handToTreeInserter(list);
 	}
+	
+public String transformPath(String path)  {
+        String ret = "";
+        String[] split = path.split("/");
+        for (int i = split.length - 1; i >= 0; i--) {
+                if (i == split.length - 1)
+                        ret = split[i] + ".";
+                else
+                        ret = split[i] + "(" + ret + ")";
+        }
+        System.out.println("TF - Path Transformed: "+ret);
+        return ret;
+}
 
 	// Giving found Subtrees and Path to the TreeInserter
 	private void handToTreeInserter(List<String> list) {
-		if (!list.isEmpty()) {
 			TreeInserter ti = new TreeInserter();
-			ti.createTrees(list);
-		}
+			//transforming into alternative coding
+			transformPath(path);
+			System.out.println("TF - Liste: "+list.toString());
+			ti.createTrees(path, list);
 	}
+	
 	/*
 	 * public List<String> getList(){ return list; }
 	 */
