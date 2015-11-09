17875470054956f8d4e8b4f0643a97de7254fb25
eugenszk <e.saizki@gmail.com>
2015-11-09 17:16:01 +0100

    Implemented algorithm for finding relevant subtrees

--- src/main/java/de/unikoblenz/west/lda/subtreeBuilder/TreeFilter.java
+++ src/main/java/de/unikoblenz/west/lda/subtreeBuilder/TreeFilter.java
@@ -1,61 +1,84 @@
 package de.unikoblenz.west.lda.subtreeBuilder;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class TreeFilter {
 	// Getting Path from TreeTraversal
-	static String path = "1/2";
+	static String path = "1/2/2/2/2/2";
 
 	// String path = getPath();
 
 	// Querying Cache for Paths including just received Path
 
-	List<String> list = new LinkedList<String>();
+	List<String> relevantTrees = new ArrayList<String>();
+	String parent = "";
 
 	public void queryCache(String path) {
-		
-		// getting results results from the cache
-		if (!path.contains("/")){
-			//return everything b/c we are looking for root node
-			//Cache.query(Nil);
-		} else
+
+		// getting results from the cache
+		if (!path.contains("/")) {
+			// Query Cache with "parent" matched
+			List<String> queriedPaths = new ArrayList<String>();
+			// queriedPaths = Cache.query(path)
+
+			for (String queried : queriedPaths) {
+				//have to split by points on highest level
+				String[] split = queried.split(".");
+				for (String splitQuery : split) {
+					if (splitQuery.matches("^" + "\\(*" + Pattern.quote(parent) + ".*")) {
+						relevantTrees.add(queried);
+					}
+				}
+			}
+
+			handToTreeInserter(relevantTrees);
+		} else {
 			path = path.substring(0, path.lastIndexOf("/"));
-		System.out.println("TF - Path: "+path);
-		
-		//query Cache for existing trees
-		//Cache.query(path)
-		
-		//Adding results from Cache to List
-		//list.addAll(Cache.query(ancestors)); 
-		
-		
-		// handing created list over to TreeInserter
-		handToTreeInserter(list);
+			// if parent does not exist, create one time
+			if (parent.isEmpty())
+				parent = path;
+
+			// query Cache for new path
+			List<String> queriedPaths = new ArrayList<String>();
+			// queriedPaths = Cache.query(path)
+
+			for (String queried : queriedPaths) {
+				if (queried.matches("^" + "\\(*" + Pattern.quote(parent) + ".*")) {
+					relevantTrees.add(queried);
+				}
+			}
+
+			System.out.println("TF - Path: " + path);
+			// System.out.println("TF - Parent: "+parent);
+			queryCache(path);
+		}
+
 	}
-	
-public String transformPath(String path)  {
-        String ret = "";
-        String[] split = path.split("/");
-        for (int i = split.length - 1; i >= 0; i--) {
-                if (i == split.length - 1)
-                        ret = split[i] + ".";
-                else
-                        ret = split[i] + "(" + ret + ")";
-        }
-        System.out.println("TF - Path Transformed: "+ret);
-        return ret;
-}
+
+	public String transformPath(String path) {
+		String ret = "";
+		String[] split = path.split("/");
+		for (int i = split.length - 1; i >= 0; i--) {
+			if (i == split.length - 1)
+				ret = split[i] + ".";
+			else
+				ret = split[i] + "(" + ret + ")";
+		}
+		// System.out.println("TF - Path Transformed: "+ret);
+		return ret;
+	}
 
 	// Giving found Subtrees and Path to the TreeInserter
 	private void handToTreeInserter(List<String> list) {
-			TreeInserter ti = new TreeInserter();
-			//transforming into alternative coding
-			transformPath(path);
-			System.out.println("TF - Liste: "+list.toString());
-			ti.createTrees(path, list);
+		TreeInserter ti = new TreeInserter();
+		// transforming into alternative coding
+		transformPath(path);
+		// System.out.println("TF - Liste: "+list.toString());
+		ti.createTrees(path, list);
 	}
-	
+
 	/*
 	 * public List<String> getList(){ return list; }
 	 */
@@ -63,5 +86,9 @@ public String transformPath(String path)  {
 	public static void main(String[] args) {
 		TreeFilter tf = new TreeFilter();
 		tf.queryCache(path);
+		// String bla = "((((1/1/3/2))))))))";
+		// String parent = "1/1";
+		// System.out.println(bla.matches("^" + "\\(*" + Pattern.quote(parent) +
+		// ".*"));
 	}
 }
