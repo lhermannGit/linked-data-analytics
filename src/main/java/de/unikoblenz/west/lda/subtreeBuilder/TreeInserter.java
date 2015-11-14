4190ba22456b1be251500c072260e54e8b7c309d
Danniene <dwete@uni-koblenz.de>
2015-11-14 13:08:49 +0100

    actual progress on treeInserter

--- src/main/java/de/unikoblenz/west/lda/subtreeBuilder/TreeInserter.java
+++ src/main/java/de/unikoblenz/west/lda/subtreeBuilder/TreeInserter.java
@@ -1,6 +1,8 @@
 package de.unikoblenz.west.lda.subtreeBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class TreeInserter {
 	//not needed right now, as createTrees gets the full list of trees
@@ -8,11 +10,51 @@ public class TreeInserter {
 	//List<String> list = TreeFilter.getList();
 	
 	//Finding correct Positions for new Path
-	public void createTrees (String path, List<String> list){
-		System.out.println("TI - Path: "+path);
-		System.out.println("TI - Liste: "+list.toString());
+	
+	public static void createTrees (String path, List<String> list){
 		
+		for(String s : list) {
+			
+			if(s.matches("^" +Pattern.quote(path) + ".*"))  {
+				List<String> newSubtrees = new ArrayList<String>();
+			    newSubtrees.add(s);
+			}
+			    //String nodeToAdd = path.substring(path.length()-1);
+			System.out.println("TI - Path: "+path);
+			System.out.println("TI - Liste: "+list.toString());
+			
+		}
 	}
+	
+	public static String insertNodeToTree(String path, String tree, int depth) {
+		
+		String nodeToAdd = path.substring(path.length()-1);
+		return null;
+	}
+			
+		
+	
+	
+	
+	//Discarding found Subtrees and saving newly created ones
+	
+	
+	public static void main(String[] args) {
+		String path0 = "1/2";
+		List <String> relevantPath = new ArrayList<String>();
+		String[] paths = {"1/4/6/7", "2/4/8.3", "1.2(3.9)"};
+		for( String path : paths)
+			relevantPath.add(path);
+		createTrees(path0, relevantPath);
+		
+		for(String s : relevantPath) {
+		System.out.println(s);
+		
+		System.out.println(s.substring(s.length()-1));
+	}
+	}
+	
+	
 	
 	//Discarding found Subtrees and saving newly created ones
 	
