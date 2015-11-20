0e8dfc9554a402fde212e7fc3cee703347b00e36
Danniene <dwete@uni-koblenz.de>
2015-11-20 13:07:07 +0100

    My actual progress on TreeInserter

--- src/main/java/de/unikoblenz/west/lda/subtreeBuilder/TreeInserter.java
+++ src/main/java/de/unikoblenz/west/lda/subtreeBuilder/TreeInserter.java
@@ -1,8 +1,6 @@
 package de.unikoblenz.west.lda.subtreeBuilder;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TreeInserter {
@@ -13,53 +11,66 @@ public class TreeInserter {
 	//Finding correct Positions for new Path
 	
 	public static void createTrees (String path, List<String> list){
-		
-		
-		for(String s : list) {
+		String parent = "";
+        String nodeToAdd = "";
+		if(path.contains("/")) {
+			int index = path.lastIndexOf("/");
+			parent = path.substring(0, index);
+			nodeToAdd = path.substring(index+1);
 			
-			if(s.matches("^" +Pattern.quote(path) + ".*"))  {
-				List<String> newSubtrees = new ArrayList<String>();
-			    newSubtrees.add(s);
-			}
-			    //String nodeToAdd = path.substring(path.length()-1);
+		}
+		int index = 0;
+		String regex = ("^" + "\\(*" + Pattern.quote(parent) + ".*");
+
+		for(String s : list) {
+			index = s.indexOf(parent);
+			String part1 = s.substring(0, index + 1);
+			String part2 = s.substring(index + 1);
+			String result = part1 + nodeToAdd + part2;
+		}
+		
+		  
+			
+			//if(s.matches("^" +Pattern.quote(path) + ".*"))  
+			    
 			System.out.println("TI - Path: "+path);
 			System.out.println("TI - Liste: "+list.toString());
 			
-		}
 	}
 	
 	
 	
-	public static String insertNodeToTree(String path, String tree, int depth) {
+	//  add path to existing subtrees.
+	public static String insertNodeToTree(String path) {
+		if(!path.contains("/")) {
+			 String nodeToAdd = path.substring(path.length()-1);
+			
+		}
 		
-		String nodeToAdd = path.substring(path.length()-1);
-		return null;
+			
+				return null;
+	
+	
+		
 	}
 			
 		
-	
-	
-	
 	//Discarding found Subtrees and saving newly created ones
 	
 	
 	public static void main(String[] args) {
-		String path0 = "1/2";
-		List <String> relevantPath = new ArrayList<String>();
-		String[] paths = {"1/4/6/7", "2/4/8.3", "1.2(3.9)"};
-		for( String path : paths)
-			relevantPath.add(path);
-		createTrees(path0, relevantPath);
+		String path = "1/2/8/4/(7.2(2.1))";
+		int index = path.lastIndexOf("/") ;
+		String parent = path.substring(0, index);
+		String child = path.substring(index+1);
+		String s = "";
 		
-		for(String s : relevantPath) {
-		System.out.println(s);
-		
-		System.out.println(s.substring(s.length()-1));
-
-		//System.out.println("end");
+		System.out.println(parent);
+		System.out.println(child);
+		System.out.println(s.indexOf(parent));
 		
 	}
 	
-	
-	
-}}
+		
+		
+		}
