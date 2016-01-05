8ba0a299f4c22d1643a816a44a98e9702dd5553d
Jule <jbrecht@uni-koblenz.de>
2016-01-05 19:23:47 +0100

    change output of TreeTraversal to ArrayList<Integer>

--- src/main/java/de/unikoblenz/west/lda/treeGeneration/ITreeTraversal.java
+++ src/main/java/de/unikoblenz/west/lda/treeGeneration/ITreeTraversal.java
@@ -1,9 +1,11 @@
 package de.unikoblenz.west.lda.treeGeneration;
 
+import java.util.ArrayList;
+
 public interface ITreeTraversal {
 
 	public void Initialize(RootNode rootNode);
 
 	// Returns null if all paths are exhausted
-	public String getNextPath();
+	public ArrayList<Integer> getNextPath();
 }
--- src/main/java/de/unikoblenz/west/lda/treeGeneration/TreeTraversal.java
+++ src/main/java/de/unikoblenz/west/lda/treeGeneration/TreeTraversal.java
@@ -1,5 +1,6 @@
 package de.unikoblenz.west.lda.treeGeneration;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -8,16 +9,17 @@ public class TreeTraversal implements ITreeTraversal {
 
 	public TreeTraversal(){
 		queueNodes = new LinkedList<Node>();
-		queuePairs = new LinkedList<String>();
-		queuePath = new LinkedList<String>();
+		queuePairs = new LinkedList<ArrayList<Integer>>();
+		queuePath = new LinkedList<ArrayList<Integer>>();
 		children = new LinkedList<ChildNode>();
+		currentPath = new ArrayList<Integer>();
 	}
 	
 	private Queue<Node> queueNodes;
-	private Queue<String> queuePath;
-	private Queue<String> queuePairs;
+	private Queue<ArrayList<Integer>> queuePath;
+	private Queue<ArrayList<Integer>> queuePairs;
 	private List<ChildNode> children;
-	private String currentPath;
+	private ArrayList<Integer> currentPath;
 	
 	
 	public void Initialize(RootNode rootNode) {
@@ -26,29 +28,39 @@ public class TreeTraversal implements ITreeTraversal {
 	    queuePairs.clear();
 	    children.clear();
 		queueNodes.add(rootNode);
-		queuePath.add("");
-	    currentPath = "";
+		queuePath.clear();
+	    currentPath.clear();
 	}
 
-	public String getNextPath() {
+	public ArrayList<Integer> getNextPath() {
+		ArrayList<Integer> helper;
 		if (queuePairs.isEmpty()){
 			while(true){
 				if (queueNodes.isEmpty()){
 					return null;
 				}
 				children = queueNodes.remove().getChildren();
-				currentPath = queuePath.remove();
+				if (!queuePath.isEmpty())
+					currentPath = queuePath.remove();
+				else
+					currentPath = null;
 				if (children.size()==0)
 					continue;
 				for (int n=0; n<children.size(); n++){
 					queueNodes.add(children.get(n));
-					queuePath.add(currentPath + n + "/");
-					queuePairs.add(currentPath + n + "/");
+					if (currentPath == null){
+						helper = new ArrayList<Integer>();
+					}
+					else{					
+						helper = new ArrayList<Integer>(currentPath);
+					}
+					helper.add(n);
+					queuePath.add(helper);
+					queuePairs.add(helper);
 				}
 				break;
 			}
 		}
 		return queuePairs.remove();
 	}
-
 }
